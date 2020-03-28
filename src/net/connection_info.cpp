040e1583a2ea070505bca5f0fa1a8d7ff9d27c4e
Tristan <code@thewoosh.me>
2020-03-28 22:50:43 +0100

    Improved timings

--- src/net/connection_info.cpp
+++ src/net/connection_info.cpp
@@ -32,24 +32,37 @@ namespace Net {
 	
 	std::string FormatTime(std::chrono::duration<int32_t, std::nano> nanoDuration) {
 		std::stringstream durationStream;
-		if (nanoDuration.count() > 1000) {
+		if (nanoDuration.count() > 1e6) {
+			auto milliDuration = std::chrono::duration_cast<std::chrono::milliseconds>(nanoDuration);
+			durationStream << milliDuration.count() << " ms";
+		} else if (nanoDuration.count() > 1e3) {
 			auto microDuration = std::chrono::duration_cast<std::chrono::microseconds>(nanoDuration);
-			if (microDuration.count() > 1000) {
-				auto milliDuration = std::chrono::duration_cast<std::chrono::nanoseconds>(microDuration);
-				durationStream << milliDuration.count() << " ms";
-			} else
-				durationStream << microDuration.count() << " μs";
+			durationStream << microDuration.count() << " μs";
 		} else
 			durationStream << nanoDuration.count() << " ns";
+
 		return durationStream.str();
 	}
 
 	bool ConnectionInfo::ResolveHostName() {
+		int errorCode;
 		struct addrinfo *address;
 		struct addrinfo *result;
+		
+		Socket = socket(AF_INET, SOCK_STREAM, 0);
+		if (Socket == -1) {
+			errorCode = errno;
+			std::stringstream information;
+			information << "Error on socket(): " << CCompat::GetErrnoName(errorCode) << " (" << errorCode << ")";
+			Logger::Warning("Net::ConnectionInfo::ResolveHostName", information.str());
+			freeaddrinfo(result);
+			return false;
+		}
 
 		auto startResolve = std::chrono::high_resolution_clock::now();
-		int errorCode = getaddrinfo(HostName.c_str(), NULL, NULL, &result);
+		errorCode = getaddrinfo(HostName.c_str(), NULL, NULL, &result);
+		TimingDNS = FormatTime(std::chrono::high_resolution_clock::now() - startResolve);
+
 		if (errorCode != 0) {
 			std::stringstream information;
 			information << "Error on getaddrinfo(): " << CCompat::GetErrnoName(errorCode) << " (" << errorCode << ")";
@@ -58,38 +71,24 @@ namespace Net {
 			return false;
 		}
 
+		auto startPing = std::chrono::high_resolution_clock::now();
 		for (address = result; address != NULL; address = address->ai_next) {
-			char hostname[1025];
-			errorCode = getnameinfo(address->ai_addr, address->ai_addrlen, hostname, 1025, NULL, 0, 0); 
-			std::cout << "trying " << hostname << std::endl;
-			Socket = socket(AF_INET, SOCK_STREAM, 0);
-			if (Socket == -1) {
-				errorCode = errno;
-				std::stringstream information;
-				information << "Error on socket(): " << CCompat::GetErrnoName(errorCode) << " (" << errorCode << ")";
-				Logger::Warning("Net::ConnectionInfo::ResolveHostName", information.str());
-				freeaddrinfo(result);
-				return false;
-			}
-
-			auto startConnect = std::chrono::high_resolution_clock::now();
-			
 			struct sockaddr_in connectAddress;
 			connectAddress.sin_addr.s_addr = ((struct sockaddr_in *) address->ai_addr)->sin_addr.s_addr;
 			connectAddress.sin_family = AF_INET;
 			connectAddress.sin_port = htons(Port);
 
+			auto startConnect = std::chrono::high_resolution_clock::now();
 			if (connect(Socket, (struct sockaddr*) &connectAddress, sizeof(struct sockaddr_in)) != -1) {
 				TimingConnect = FormatTime(std::chrono::high_resolution_clock::now() - startConnect);
-				// TimingDNS shouldn't count the time of the successful connect().
-				TimingDNS = FormatTime(startConnect - startResolve);
+				TimingPing = FormatTime(startConnect - startPing);
 				freeaddrinfo(result);
 				return true;
 			}
-
-			close(Socket);
 		}
+		TimingPing = FormatTime(std::chrono::high_resolution_clock::now() - startPing);
 		freeaddrinfo(result);
+		close(Socket);
 
 		Socket = 0;
 		TimingConnect = "0 ms (failed)";
@@ -107,6 +106,8 @@ namespace Net {
 			Logger::Warning("Net::ConnectionInfo::Connect", "Failed to connect!");
 			return false;
 		}
+
+		Logger::Debug("Net::ConnectionInfo::Connect", "Timings:\n\tDNS Resolving:             " + TimingDNS + "\n\tPinging other hosts:       " + TimingPing + "\n\tConnection Establishment:  " + TimingConnect);
 
 		// Other settings? Such as tls?
 		return true;
--- src/net/connection_info.hpp
+++ src/net/connection_info.hpp
@@ -7,12 +7,25 @@
 namespace Net {
 	class ConnectionInfo {
 	public: // Properties
+		/* The HostName, as entered by the client. */
 		std::string HostName;
+
+		/* The port it's connected to (usually 443 or 80). */
 		uint16_t Port;
+
+		/* Is the connection wrapped in TLS? */
 		bool Secure;
 
+		/* The time it took to resolve the hostname. */
 		std::string TimingDNS;
+
+		/* The time that was wasted trying to find a connectable server.
+		 * (TimingConnect excluded) */
+		std::string TimingPing;
+
+		/* The time it took to connect to the currently-connnected-to server. */
 		std::string TimingConnect;
+
 	private: // Private Properties
 		int Socket;
 	public: // Methods
