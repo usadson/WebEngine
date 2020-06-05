a85d7b0cee7d03f9435d6d48bab468e0b4903c45
Tristan <code@thewoosh.me>
2020-06-05 22:11:10 +0200

    [parser/html] Ported DOCTYPE_NAME

--- Makefile
+++ Makefile
@@ -88,6 +88,7 @@ BINARIES = bin/ccompat.o \
 	   bin/parser/html/tokenizer/comment_start_dash.o \
 	   bin/parser/html/tokenizer/comment.o \
 	   bin/parser/html/tokenizer/data.o \
+	   bin/parser/html/tokenizer/doctype_name.o \
 	   bin/parser/html/tokenizer/doctype.o \
 	   bin/parser/html/tokenizer/markup_declaration_open.o \
 	   bin/parser/html/tokenizer/self_closing_start.o \
@@ -413,6 +414,13 @@ bin/parser/html/tokenizer/comment.o: parser/html/tokenizer/comment.cpp \
 	parser/html/token.hpp \
 	parser/html/tokenizer.hpp
 	$(CXX) $(CXXFLAGS) -c -o $@ parser/html/tokenizer/comment.cpp
+
+bin/parser/html/tokenizer/doctype_name.o: parser/html/tokenizer/doctype_name.cpp \
+	parser/html/tokenizer/doctype_name.hpp \
+	parser/html/context.hpp \
+	parser/html/token.hpp \
+	parser/html/tokenizer.hpp
+	$(CXX) $(CXXFLAGS) -c -o $@ parser/html/tokenizer/doctype_name.cpp
 
 bin/parser/html/tokenizer/doctype.o: parser/html/tokenizer/doctype.cpp \
 	parser/html/tokenizer/doctype.hpp \
---
+++ parser/html/tokenizer/doctype_name.cpp
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) 2020 Tristan. All Rights Reserved.
+ * This file is licensed under the BSD 2-Clause license.
+ * See the COPYING file for licensing information.
+ */
+
+#include "doctype_name.hpp"
+
+#include <iostream>
+#include <vector>
+
+bool
+HTML::Tokenizer::DoctypeName::Parse() {
+	if (context.eof) {
+		context.LogError(HTML::Tokenizer::ParserError::EOF_IN_DOCTYPE);
+		tokenizer.treeConstructor.EmitDoctypeQuirksToken();
+		tokenizer.treeConstructor.EmitEOFToken();
+	} else {
+		switch (context.character) {
+			case '\t':
+			case '\n':
+			case '\f':
+			case ' ':
+				context.state = HTML::Tokenizer::ParserState::AFTER_DOCTYPE_NAME;
+				break;
+			case '>':
+				std::cout << "Doctype name: " << context.doctypeToken.name.value() << std::endl;
+				tokenizer.treeConstructor.EmitToken(context.doctypeToken);
+				context.doctypeToken = HTML::Tokenizer::DoctypeToken(); // reset
+				context.state = HTML::Tokenizer::ParserState::DATA;
+				break;
+			case '\0':
+				context.LogError(HTML::Tokenizer::ParserError::UNEXPECTED_NULL_CHARACTER);
+				context.doctypeToken.name = context.doctypeToken.name.value() + Unicode::REPLACEMENT_CHARACTER;
+				break;
+			default:
+				if (context.character >= 0x41 && context.character <= 0x5A) {// Is uppercase
+					context.doctypeToken.name = context.doctypeToken.name.value() + static_cast<char>(context.character + 0x20);;
+				} else {
+					context.doctypeToken.name = context.doctypeToken.name.value() + context.character;
+				}
+				break;
+		}
+	}
+
+	return true;
+}
