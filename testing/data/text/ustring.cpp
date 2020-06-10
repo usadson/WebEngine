2e7db38f6888b4a55719d7bae70b1120019f0eef
Tristan <code@thewoosh.me>
2020-06-10 16:12:45 +0200

    [Testing] Added more test cases

--- testing/data/text/ustring.cpp
+++ testing/data/text/ustring.cpp
@@ -18,7 +18,31 @@ namespace Unicode {
 	};
 
 	TEST_F(UStringTest, ValidInputs) {
-		ASSERT_EQ(normalString.length(), 11);
+		ASSERT_EQ(normalString.length(), 11)
+			<< "Invalid size detected";
+
+		ASSERT_TRUE(normalString.EqualsIgnoreCaseA(0, "HELLO WORLD"));
+		ASSERT_TRUE(normalString.EqualsIgnoreCaseA(0, "hello world"));
+		ASSERT_TRUE(normalString.EqualsIgnoreCaseA(0, "hElLo wOrLd"));
+		ASSERT_TRUE(normalString.EqualsIgnoreCaseA(0, "hELLo wORLd"));
+
+		ASSERT_TRUE(normalString.EqualsIgnoreCaseA(6, "WORLD"));
+		ASSERT_TRUE(normalString.EqualsIgnoreCaseA(6, "world"));
+		ASSERT_TRUE(normalString.EqualsIgnoreCaseA(6, "World"));
+		ASSERT_TRUE(normalString.EqualsIgnoreCaseA(6, "wOrLD"));
+
+		ASSERT_FALSE(normalString.EqualsIgnoreCaseA(0, "another string with another length"));
+		ASSERT_FALSE(normalString.EqualsIgnoreCaseA(0, "sameLength"));
+		ASSERT_FALSE(normalString.EqualsIgnoreCaseA(0, ""));
+
+		ASSERT_TRUE(normalString.EqualsA("Hello world"));
+		ASSERT_TRUE(normalString.EqualsAL(0, "Hello world", 11));
+
+		ASSERT_FALSE(normalString.EqualsA("Helo world"));
+		ASSERT_FALSE(normalString.EqualsA(""));
+
+		ASSERT_FALSE(normalString.EqualsAL(0, "nope", 4));
+		ASSERT_FALSE(normalString.EqualsAL(0, "", 0));
 	}
 
 } // namespace TextEncoding
