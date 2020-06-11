c622aec924e2e0e841e64f8a9e12a6b1e5fb04a5
Tristan <code@thewoosh.me>
2020-06-11 15:15:02 +0200

    [DOM] Added head element

---
+++ dom/html_head_element.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+/**
+ * Copyright (C) 2020 Tristan. All Rights Reserved.
+ * This file is licensed under the BSD 2-Clause license.
+ * See the COPYING file for licensing information.
+ */
+
+#include <map>
+#include <memory>
+
+#include "dom/html_element.hpp"
+
+namespace DOM {
+
+	// The <head> element
+	// Specification:
+	// https://html.spec.whatwg.org/multipage/semantics.html#the-head-element
+	class HTMLHeadElement : public HTMLElement {
+	public:
+	};
+} // namespace DOM
