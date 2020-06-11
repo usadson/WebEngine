9280706178ec617393e8bb0119d0205d018418c4
Tristan <code@thewoosh.me>
2020-06-11 23:59:14 +0200

    [HTMLParser] Let logging be through the Logger class.

--- parser/html/context.cpp
+++ parser/html/context.cpp
@@ -6,20 +6,23 @@
 
 #include "context.hpp"
 
-#include <iostream>
+#include <sstream>
 
 #include "logger.hpp"
 
 void
 HTML::Tokenizer::Context::LogError(const HTML::Tokenizer::ParserError &error) {
-	std::cerr << "\033[1;31mParserError: \033[1;33m" << error.name << "\033[1;31m at \033[1;35m" << lineCount << ':'
+	std::stringstream info;
+	info << "\033[1;31mParserError: \033[1;33m" << error.name << "\033[1;31m at \033[1;35m" << lineCount << ':'
 			  << linePosition << "\033[1;31m in state \033[1;36m" << state;
 
 	if (beginLoopState != state)
-		std::cerr << "\033[1;31m (begin was: \033[1;33m" << beginLoopState << ')';
+		info << "\033[1;31m (begin was: \033[1;33m" << beginLoopState << ')';
 
-	std::cerr << "\033[1;31m (character: '" << currentCharacter << "' i.e. 0x" << std::hex << (size_t)currentCharacter
-			  << std::dec << ")\033[1;0m" << std::endl;
+	info << "\033[1;31m (character: '" << currentCharacter << "' i.e. 0x" << std::hex << (size_t)currentCharacter
+			  << std::dec << ")\033[1;0m";
+
+	Logger::Error("HTMLTokenizer", info.str());
 }
 
 void
