2bd64e20620a694315cf4282e41908078c6db8e0
Tristan <code@thewoosh.me>
2020-06-12 00:26:39 +0200

    [Style] Formatted parser/html/context.hpp

--- parser/html/context.cpp
+++ parser/html/context.cpp
@@ -14,13 +14,13 @@ void
 HTML::Tokenizer::Context::LogError(const HTML::Tokenizer::ParserError &error) {
 	std::stringstream info;
 	info << "\033[1;31mParserError: \033[1;33m" << error.name << "\033[1;31m at \033[1;35m" << lineCount << ':'
-			  << linePosition << "\033[1;31m in state \033[1;36m" << state;
+		 << linePosition << "\033[1;31m in state \033[1;36m" << state;
 
 	if (beginLoopState != state)
 		info << "\033[1;31m (begin was: \033[1;33m" << beginLoopState << ')';
 
-	info << "\033[1;31m (character: '" << currentCharacter << "' i.e. 0x" << std::hex << static_cast<std::size_t>(currentCharacter)
-			  << std::dec << ")\033[1;0m";
+	info << "\033[1;31m (character: '" << currentCharacter << "' i.e. 0x" << std::hex
+		 << static_cast<std::size_t>(currentCharacter) << std::dec << ")\033[1;0m";
 
 	Logger::Error("HTMLTokenizer", info.str());
 }
