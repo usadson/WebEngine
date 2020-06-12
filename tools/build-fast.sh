d3776516e41d44ba104e6d30b2569100330db547
Tristan <code@thewoosh.me>
2020-06-12 15:08:02 +0200

    [Tools] Added copyright info to tools/build-fast.sh

--- tools/build-fast.sh
+++ tools/build-fast.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/bash
+#
+# Copyright (C) 2020 Tristan. All Rights Reserved.
+# This file is licensed under the BSD 2-Clause license.
+# See the COPYING file for licensing information.
+
 make bin/test.txt
 make -j`grep -c ^processor /proc/cpuinfo | awk '{print $1 * 1.5}'` objects
 make
