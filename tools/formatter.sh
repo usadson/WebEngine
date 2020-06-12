2c850df65126ba4f000994c1e640a4ff73677b93
Tristan <code@thewoosh.me>
2020-06-12 15:17:16 +0200

    [Tools] Added tools/formatter.sh

---
+++ tools/formatter.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# Copyright (C) 2020 Tristan. All Rights Reserved.
+# This file is licensed under the BSD 2-Clause license.
+# See the COPYING file for licensing information.
+find . -iname '*.hpp' -o -iname '*.cpp' | xargs clang-format -i
