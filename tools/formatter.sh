857c65b4742f18c4c4e99b63b77b61eb631627b8
Tristan <code@thewoosh.me>
2020-06-12 15:21:10 +0200

    [Tools] Improved formatter script

--- tools/formatter.sh
+++ tools/formatter.sh
@@ -3,4 +3,4 @@
 # Copyright (C) 2020 Tristan. All Rights Reserved.
 # This file is licensed under the BSD 2-Clause license.
 # See the COPYING file for licensing information.
-find . -iname '*.hpp' -o -iname '*.cpp' | xargs clang-format -i
+find . -iname '*.hpp' -o -iname '*.cpp' -exec clang-format -i {} +
