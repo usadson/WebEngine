8284183a4556d3fdaefda85fc93a0a34273d8241
Tristan <code@thewoosh.me>
2020-06-16 00:16:14 +0200

    [Garbage] Intentional style issues

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@
  */
 
 #include <iostream>
-#include <memory>
+  #include <memory>
 #include <sstream>
 #include <vector>
 
@@ -50,7 +50,7 @@ DecodeText(Resources::DocumentResource *documentResource, std::vector<char> inpu
 			return false;
 		}
 
-		documentResource->data = Unicode::UString(utf8Encoding.Output);
+                                		documentResource->data = Unicode::UString(utf8Encoding.Output);
 		return true;
 	}
 
