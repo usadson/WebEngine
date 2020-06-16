b744f9306021f8a3e068a28a48e85831657f8b5e
Tristan <code@thewoosh.me>
2020-06-17 00:12:28 +0200

    [CodeQuality] Improved namespace-decls in http_response_info.cpp

--- net/http/http_response_info.cpp
+++ net/http/http_response_info.cpp
@@ -11,32 +11,30 @@
 
 #include <cstring>
 
-namespace Net {
-	namespace HTTP {
-		std::optional<const char *>
-		HTTPResponseInfo::GetHeader(const char *name) const {
-			auto result = std::find_if(std::begin(headers),
-				std::end(headers),
-				[name](const HTTPHeaderField &headerField) -> bool {
-					return strcasecmp(headerField.fieldName.c_str(), name) == 0;
-				});
+namespace Net::HTTP {
+	std::optional<const char *>
+	HTTPResponseInfo::GetHeader(const char *name) const {
+		auto result = std::find_if(std::begin(headers),
+			std::end(headers),
+			[name](const HTTPHeaderField &headerField) -> bool {
+				return strcasecmp(headerField.fieldName.c_str(), name) == 0;
+			});
 
-			if (result == std::end(headers))
-				return std::optional<const char *>();
-			else
-				return std::optional<const char *>(result->fieldValue.c_str());
-		}
+		if (result == std::end(headers))
+			return std::optional<const char *>();
+		else
+			return std::optional<const char *>(result->fieldValue.c_str());
+	}
 
-		std::optional<std::size_t>
-		HTTPResponseInfo::GetHeaderUnsigned(const char *name) const {
-			std::size_t res;
-			std::optional<const char *> value;
+	std::optional<std::size_t>
+	HTTPResponseInfo::GetHeaderUnsigned(const char *name) const {
+		std::size_t res;
+		std::optional<const char *> value;
 
-			value = GetHeader(name);
-			if (value.has_value() && sscanf(value.value(), "%zu", &res) == 1)
-				return res;
+		value = GetHeader(name);
+		if (value.has_value() && sscanf(value.value(), "%zu", &res) == 1)
+			return res;
 
-			return std::optional<std::size_t>();
-		}
-	} // namespace HTTP
-} // namespace Net
+		return std::optional<std::size_t>();
+	}
+} // namespace Net::HTTP
