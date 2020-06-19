97d6d70f834206a24a5fb29a9fc9e8fec419d421
Tristan <code@thewoosh.me>
2020-06-19 22:54:37 +0200

    [Testing] Added unicode.hpp test suite

---
+++ testing/data/text/unicode.cpp
@@ -0,0 +1,32 @@
+/**
+ * Copyright (C) 2020 Tristan. All Rights Reserved.
+ * This file is licensed under the BSD 2-Clause license.
+ * See the COPYING file for licensing information.
+ */
+
+#include "data/text/ustring.hpp"
+
+#include <climits>
+
+#include <gtest/gtest.h>
+
+namespace Unicode {
+	TEST(Unicode, ToLowerASCII) {
+		ASSERT_EQ(ToLowerASCII('a'), 'a');
+		ASSERT_EQ(ToLowerASCII('q'), 'q');
+		ASSERT_EQ(ToLowerASCII('z'), 'z');
+
+		ASSERT_EQ(ToLowerASCII('A'), 'a');
+		ASSERT_EQ(ToLowerASCII('Q'), 'q');
+		ASSERT_EQ(ToLowerASCII('Z'), 'z');
+
+		ASSERT_EQ(ToLowerASCII('0'), '0');
+		ASSERT_EQ(ToLowerASCII(0xDEADBEEF), 0xDEADBEEF);
+	}
+}
+
+int
+main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
