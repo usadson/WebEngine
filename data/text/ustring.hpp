b9a64f91afdaf23895f0b69a6d34c07f07454179
Tristan <code@thewoosh.me>
2020-06-20 16:24:54 +0200

    [UString] Improve header

--- data/text/ustring.hpp
+++ data/text/ustring.hpp
@@ -14,31 +14,47 @@
 #include "unicode.hpp"
 
 namespace Unicode {
+	/**
+	 *                                [Warning]
+	 * Using case-insensitive functions only works with ASCII characters. These
+	 * are characters from the ISO basic Latin alphabet*. These functions are
+	 * useful for example for parsing languages with English keywords, like
+	 * HTML. Do not use these anywhere except where standards explicitly
+	 * permits use. This is to avoid internationalization bugs.
+	 *
+	 *                                [Warning]
+	 * Using `const char *` inputs is only permitted on ASCII strings. If you
+	 * want to use non-ASCII strings or characters inside a UString, use a
+	 * TextEncoding::Encoding** for the former, and an Unicode::CodePoint***
+	 * for thelatter.
+	 *
+	 * *   Wikipedia article:
+	 *     https://en.wikipedia.org/wiki/ISO_basic_Latin_alphabet
+	 * **  See the declaration in: data/text/encoder/encoding.hpp
+	 * *** See the declaration in: data/text/unicode.hpp
+	 */
 	class UString {
 		/**
 		 * The following property should be private but since the functions outside
 		 * this class (inside namespace Unicode) need this data.
 		 */
-	  public: // Private Property
+	public: // Private Property
 		std::vector<Unicode::CodePoint> data;
 
-	  public: // Constructor Methods
+	public: // Constructor Methods
 		UString() noexcept;
 
 		explicit UString(std::vector<Unicode::CodePoint> characters) noexcept : data(std::move(characters)) {
 		}
 
-		// cppcheck-suppress[noExplicitConstructor]
 		explicit UString(Unicode::CodePoint) noexcept;
 
-		/* Only for ASCII characters: */
 		explicit UString(const char *, std::size_t) noexcept;
 
-		// cppcheck-suppress noExplicitConstructor
 		inline explicit UString(const char *ascii) noexcept : UString(ascii, strlen(ascii)) {
 		}
 
-	  public: // Methods
+		/*** Methods ****/
 		[[nodiscard]] inline const Unicode::CodePoint &
 		operator[](std::size_t index) const noexcept {
 			return data[index];
@@ -57,16 +73,15 @@ namespace Unicode {
 			return !operator==(other);
 		}
 
-		UString &
-		operator+=(const Unicode::CodePoint) noexcept;
-
 		[[nodiscard]] UString
 		operator+(const Unicode::CodePoint &) const noexcept;
 
 		[[nodiscard]] UString
 		operator+(const UString &) const noexcept;
 
-		/** Only for strictly-ASCII related strings. **/
+		UString &
+		operator+=(const Unicode::CodePoint) noexcept;
+
 		/* Append a strictly-ASCII string */
 		UString &
 		operator+=(const UString &) noexcept;
@@ -75,9 +90,15 @@ namespace Unicode {
 		UString &
 		operator+=(const char *) noexcept;
 
-		/* Is the character at position <std::size_t> an ASCII character? */
 		[[nodiscard]] bool
-		IsASCIIAlpha(std::size_t) const noexcept;
+		EqualsA(const char *) const noexcept;
+
+		/* Equals case-sensitivily ASCII at index + length */
+		[[nodiscard]] bool
+		EqualsAL(std::size_t, const char *, std::size_t) const noexcept;
+
+		[[nodiscard]] bool
+		EqualsIgnoreCase(const UString &other) const noexcept;
 
 		/* Equals Ignore-case ASCII at index */
 		[[nodiscard]] bool
@@ -87,30 +108,24 @@ namespace Unicode {
 		[[nodiscard]] bool
 		EqualsIgnoreCaseAL(std::size_t, const char *, std::size_t) const noexcept;
 
-		/* Equals case-sensitivily ASCII at index + length */
+		/* Is the character at position <std::size_t> an ASCII character? */
 		[[nodiscard]] bool
-		EqualsAL(std::size_t, const char *, std::size_t) const noexcept;
-
-		[[nodiscard]] bool
-		EqualsA(const char *) const noexcept;
+		IsASCIIAlpha(std::size_t) const noexcept;
 
 		[[nodiscard]] bool
 		StartsWithA(const char *) const noexcept;
 
 		[[nodiscard]] bool
 		StartsWithIgnoreCaseAL(std::size_t pos, const char *ascii, size_t length) const noexcept;
-
-		[[nodiscard]] bool
-		EqualsIgnoreCase(const UString &other) const noexcept;
 	};
 
 	std::ostream &
 	operator<<(std::ostream &stream, const UString &string);
 
-	int
-	CompareStatic(const UString &lhs, const UString &rhs) noexcept;
-
 	bool
 	operator<(const UString &lhs, const UString &rhs) noexcept;
+
+	int
+	CompareStatic(const UString &lhs, const UString &rhs) noexcept;
 
 } // namespace Unicode
