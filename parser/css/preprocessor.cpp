c40a6b34ec7e7a54a469b2f75225477d7014a84f
Tristan <code@thewoosh.me>
2020-08-02 17:35:06 +0200

    Revert "[CI] Add Coveralls"
    This reverts commit 42d034df4406dcdb92d6456b239a1d7aed1d1280.

--- parser/css/preprocessor.cpp
+++ parser/css/preprocessor.cpp
@@ -8,22 +8,8 @@
 
 namespace CSS {
 
-	// Tasks:
-	// - Replace CRLF, CR, FF with a single LF
-	// - Replace NULL with 0xFFFD
 	bool
 	Preprocessor::Run(Unicode::UString &string) const noexcept {
-		for (std::size_t i = 0; i < string.length(); i++) {
-			if (string[i] == '\r') {
-				if (string.length() > i + 1 && string[i] == '\n') {
-// 					string[i] = '\n';
-// 					string.
-				} else {
-// 					string[i] = '\n';
-				}
-			}
-		}
-
 		return true;
 	}
 
