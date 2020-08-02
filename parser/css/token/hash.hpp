f886c4b65b4bcdf418ed8844cd90d9d8a1375002
Tristan <code@thewoosh.me>
2020-08-03 01:00:28 +0200

    [CSS] Add <hash> Token specializations

---
+++ parser/css/token/hash.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+/**
+ * Copyright (C) 2020 Tristan. All Rights Reserved.
+ * This file is licensed under the BSD 2-Clause license.
+ * See the COPYING file for licensing information.
+ */
+
+#include <vector>
+
+#include "data/text/unicode.hpp"
+#include "parser/css/token.hpp"
+#include "parser/css/token_flag.hpp"
+#include "parser/css/token_type.hpp"
+
+// CSS Parse Tokens
+//
+// Specialization of <hash-token>
+//
+// Defined normatively by:
+// https://www.w3.org/TR/css-syntax-3/#tokenization
+
+namespace CSS {
+
+	template<TokenFlag flag>
+	class Token<TokenType::HASH, flag> {
+	  public:
+		using CharVector = std::vector<Unicode::CodePoint>;
+
+		Token(const CharVector &vec) : vec(vec) {
+		}
+
+		[[nodiscard]] const CharVector &
+		Data() const noexcept {
+			return vec;
+		}
+
+	  private:
+		CharVector vec;
+	};
+
+} // namespace CSS
