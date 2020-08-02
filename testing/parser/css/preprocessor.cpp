b3c35ecc3ec4e071d6091c8c0fd6907c9a623835
Tristan <code@thewoosh.me>
2020-08-02 17:47:20 +0200

    [Testing] Add CSSPARSER ingredients

---
+++ testing/parser/css/preprocessor.cpp
@@ -0,0 +1,33 @@
+/**
+ * Copyright (C) 2020 Tristan. All Rights Reserved.
+ * This file is licensed under the BSD 2-Clause license.
+ * See the COPYING file for licensing information.
+ */
+
+#include <climits>
+
+#include <gtest/gtest.h>
+
+#include "data/text/ustring.cpp"
+#include "parser/css/preprocessor.cpp"
+
+namespace CSS {
+
+	class PreprocessorTest : public ::testing::Test {
+	  public:
+		Preprocessor instance;
+	};
+
+	TEST_F(PreprocessorTest, RunSimple) {
+		Unicode::UString string{};
+
+		ASSERT_TRUE(instance.Run(string));
+	}
+
+} // namespace CSS
+
+int
+main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
