5dc022be5e1ebbf7e6450176b8db94327f116def
Tristan <code@thewoosh.me>
2020-08-09 21:57:42 +0200

    [Test] (CSS::Tokenizer::ConsumeComments) Add CSS::Context to Tokenizer

--- testing/parser/css/tokenizer_consumecomments.cpp
+++ testing/parser/css/tokenizer_consumecomments.cpp
@@ -14,11 +14,14 @@
 
 namespace CSS {
 
-	class TokenizerConsumeCommentsTest : public ::testing::Test {};
+	class TokenizerConsumeCommentsTest : public ::testing::Test {
+	  public:
+		Context context;
+	};
 
 	TEST_F(TokenizerConsumeCommentsTest, TestEmpty) {
 		const Unicode::UString string;
-		Tokenizer tokenizer(string);
+		Tokenizer tokenizer(context, string);
 		ASSERT_TRUE(tokenizer.ConsumeComments());
 		ASSERT_TRUE(tokenizer.string.Empty());
 	}
@@ -26,7 +29,7 @@ namespace CSS {
 	TEST_F(TokenizerConsumeCommentsTest, TestNoCommentCharacters) {
 		const Unicode::UString string("No comment characters are in here");
 		const std::size_t length = string.length();
-		Tokenizer tokenizer(string);
+		Tokenizer tokenizer(context, string);
 		ASSERT_TRUE(tokenizer.ConsumeComments());
 		ASSERT_EQ(tokenizer.string.length(), length);
 	}
@@ -34,7 +37,7 @@ namespace CSS {
 	TEST_F(TokenizerConsumeCommentsTest, TestNoComments) {
 		const Unicode::UString string("/N*o comments are in her*e/");
 		const std::size_t length = string.length();
-		Tokenizer tokenizer(string);
+		Tokenizer tokenizer(context, string);
 		ASSERT_TRUE(tokenizer.ConsumeComments());
 		ASSERT_EQ(tokenizer.string.length(), length);
 	}
@@ -42,7 +45,7 @@ namespace CSS {
 	TEST_F(TokenizerConsumeCommentsTest, TestNoCommentsSlashStarSlash) {
 		const Unicode::UString string("/*/");
 		const Unicode::UString testString("Test string $iaf259");
-		Tokenizer tokenizer(string);
+		Tokenizer tokenizer(context, string);
 		tokenizer.string = testString;
 		ASSERT_FALSE(tokenizer.ConsumeComments());
 		ASSERT_EQ(tokenizer.string, testString);
@@ -50,14 +53,14 @@ namespace CSS {
 
 	TEST_F(TokenizerConsumeCommentsTest, TestEmptyComment) {
 		const Unicode::UString string("/**/");
-		Tokenizer tokenizer(string);
+		Tokenizer tokenizer(context, string);
 		ASSERT_TRUE(tokenizer.ConsumeComments());
 		ASSERT_EQ(tokenizer.string.length(), 0);
 	}
 
 	TEST_F(TokenizerConsumeCommentsTest, TestCommentWithText) {
 		const Unicode::UString string("/* Hello this is a comment*/");
-		Tokenizer tokenizer(string);
+		Tokenizer tokenizer(context, string);
 		ASSERT_TRUE(tokenizer.ConsumeComments());
 		ASSERT_EQ(tokenizer.string.length(), 0);
 	}
@@ -65,7 +68,7 @@ namespace CSS {
 	TEST_F(TokenizerConsumeCommentsTest, TestCommentStart) {
 		const Unicode::UString input("/* The following is a thing with leaves */Tree");
 		const Unicode::UString output("Tree");
-		Tokenizer tokenizer(input);
+		Tokenizer tokenizer(context, input);
 		ASSERT_TRUE(tokenizer.ConsumeComments());
 		ASSERT_EQ(tokenizer.string, output);
 	}
@@ -73,7 +76,7 @@ namespace CSS {
 	TEST_F(TokenizerConsumeCommentsTest, TestCommentMiddle) {
 		const Unicode::UString input("Tr/* unk? */ee");
 		const Unicode::UString output("Tree");
-		Tokenizer tokenizer(input);
+		Tokenizer tokenizer(context, input);
 		ASSERT_TRUE(tokenizer.ConsumeComments());
 		ASSERT_EQ(tokenizer.string, output);
 	}
@@ -81,7 +84,7 @@ namespace CSS {
 	TEST_F(TokenizerConsumeCommentsTest, TestCommentEnd) {
 		const Unicode::UString input("Tree/*s are beautiful*/");
 		const Unicode::UString output("Tree");
-		Tokenizer tokenizer(input);
+		Tokenizer tokenizer(context, input);
 		ASSERT_TRUE(tokenizer.ConsumeComments());
 		ASSERT_EQ(tokenizer.string, output);
 	}
@@ -89,7 +92,7 @@ namespace CSS {
 	TEST_F(TokenizerConsumeCommentsTest, TestCommentMultipleEnds) {
 		const Unicode::UString input("/*Comment*/*/");
 		const Unicode::UString output("*/");
-		Tokenizer tokenizer(input);
+		Tokenizer tokenizer(context, input);
 		ASSERT_TRUE(tokenizer.ConsumeComments());
 		ASSERT_EQ(tokenizer.string, output);
 	}
@@ -97,7 +100,7 @@ namespace CSS {
 	TEST_F(TokenizerConsumeCommentsTest, TestCommentMultipleEnds2) {
 		const Unicode::UString input("/*Comment*/ Test*/");
 		const Unicode::UString output(" Test*/");
-		Tokenizer tokenizer(input);
+		Tokenizer tokenizer(context, input);
 		ASSERT_TRUE(tokenizer.ConsumeComments());
 		ASSERT_EQ(tokenizer.string, output);
 	}
@@ -105,7 +108,7 @@ namespace CSS {
 	TEST_F(TokenizerConsumeCommentsTest, TestCommentMultipleBeginnings) {
 		const Unicode::UString input("/*/**/");
 		const Unicode::UString output("");
-		Tokenizer tokenizer(input);
+		Tokenizer tokenizer(context, input);
 		ASSERT_TRUE(tokenizer.ConsumeComments());
 		ASSERT_EQ(tokenizer.string, output);
 	}
@@ -113,7 +116,7 @@ namespace CSS {
 	TEST_F(TokenizerConsumeCommentsTest, TestCommentNested) {
 		const Unicode::UString input("/*/**/*/");
 		const Unicode::UString output("*/");
-		Tokenizer tokenizer(input);
+		Tokenizer tokenizer(context, input);
 		ASSERT_TRUE(tokenizer.ConsumeComments());
 		ASSERT_EQ(tokenizer.string, output);
 	}
@@ -121,7 +124,7 @@ namespace CSS {
 	TEST_F(TokenizerConsumeCommentsTest, TestCommentEOF) {
 		const Unicode::UString input("The following/* is faulty!");
 		const Unicode::UString testString("Random string #2gjs");
-		Tokenizer tokenizer(input);
+		Tokenizer tokenizer(context, input);
 		tokenizer.string = testString;
 		ASSERT_FALSE(tokenizer.ConsumeComments());
 		ASSERT_EQ(tokenizer.string, testString);
