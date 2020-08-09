1e31c17257a59a71f3191f1191468f34c2fddb51
Tristan <code@thewoosh.me>
2020-08-09 22:05:44 +0200

    [CSS] (ParseError) Pass primitive type
    Use this type for performance reasons, we can use a map to store the information about this parse error seperately.

--- parser/css/parse_error.hpp
+++ parser/css/parse_error.hpp
@@ -8,24 +8,9 @@
 
 namespace CSS {
 
-	struct ParseError {
-		const char *name;
-		const char *description;
-		const char *standardModule;
-		const char *standardModuleName;
-		const char *url;
+	enum class ParseError {
+		INVALID,
+		EOF_IN_CONSUMING_COMMENT
 	};
 
-} // namespace CSS::ParseError
-
-namespace CSS::ParseErrors {
-
-	constexpr ParseError EOFInConsumingComment{
-		"eof-in-consuming-comment",
-		"EOF found whilst consuming comment (i.e. the comment isn't ended correctly!",
-		"4.3.2.",
-		"Consume comments",
-		"https://www.w3.org/TR/css-syntax-3/#consume-comment"
-	};
-
-} // namespace CSS::ParseErrors
+} // namespace CSS
