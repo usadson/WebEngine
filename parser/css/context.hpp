d2331e79c54401208aa0d6b8a5a17162fed410f5
Tristan <code@thewoosh.me>
2020-08-09 21:56:29 +0200

    [CSS] (Context) Initial Commit

---
+++ parser/css/context.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+/**
+ * Copyright (C) 2020 Tristan. All Rights Reserved.
+ * This file is licensed under the BSD 2-Clause license.
+ * See the COPYING file for licensing information.
+ */
+
+#include "parser/css/parse_error.hpp"
+
+namespace CSS {
+
+	class Context {
+	  public:
+		inline void
+		ReportParseError(const ParseError &error) const noexcept {
+			if (errorReporter != nullptr) {
+				(*errorReporter)(error);
+			}
+		}
+
+		void (*errorReporter)(const ParseError &){ nullptr };
+	};
+
+} // namespace CSS
