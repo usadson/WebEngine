a1c29450d8c1e82dc94e37b4726f90895c74837b
Tristan <code@thewoosh.me>
2020-08-09 17:40:06 +0200

    [CSS] (TokenizerStream) Initial Commit

---
+++ parser/css/tokenizer_stream.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+/**
+ * Copyright (C) 2020 Tristan. All Rights Reserved.
+ * This file is licensed under the BSD 2-Clause license.
+ * See the COPYING file for licensing information.
+ */
+
+#include "data/text/ustring.hpp"
+
+namespace CSS {
+	class TokenizerStream {
+	  public:
+		TokenizerStream(const Unicode::UString &string) noexcept;
+
+		// Gets the next value if possible.
+		// Will consume the character.
+		[[nodiscard]] bool
+		Next(char *destination) noexcept;
+
+		[[nodiscard]] bool
+		Peek(char *destination, std::size_t offset = 0) noexcept;
+
+	  private:
+		const Unicode::UString &string;
+		std::size_t position{ 0 };
+	};
+} // namespace CSS
+
