76ee07cb94676b4ad284ff54e96cc910780ac80c
Tristan <code@thewoosh.me>
2020-08-09 18:55:07 +0200

    [Test] (CSS/TokenizerStream) Add tests

--- testing/parser/css/tokenizer_stream.cpp
+++ testing/parser/css/tokenizer_stream.cpp
@@ -27,6 +27,40 @@ namespace CSS {
 		ASSERT_FALSE(stream.Next(&destination));
 	}
 
+	TEST_F(TokenizerStreamTest, TestNextSemantics) {
+		Unicode::UString test("ABC");
+		CSS::TokenizerStream stream(test);
+
+		ASSERT_TRUE(stream.Next(&destination));
+		ASSERT_EQ(destination, test[0]);
+		ASSERT_TRUE(stream.Next(&destination));
+		ASSERT_EQ(destination, test[1]);
+		ASSERT_TRUE(stream.Next(&destination));
+		ASSERT_EQ(destination, test[2]);
+		ASSERT_FALSE(stream.Next(&destination));
+		ASSERT_EQ(destination, test[2]);
+	}
+
+	TEST_F(TokenizerStreamTest, TestPeekSame) {
+		Unicode::UString test("   ");
+		std::generate(std::begin(test), std::end(test), []() {
+			return 'A' + (random() % 26);
+		});
+		CSS::TokenizerStream stream(test);
+
+		char peek = '\0';
+		char next = '\0';
+		for (std::size_t i = 0; i < test.length(); i++) {
+			ASSERT_TRUE(stream.Peek(&peek));
+			ASSERT_TRUE(stream.Next(&next));
+			ASSERT_EQ(peek, next);
+		}
+
+		ASSERT_FALSE(stream.Peek(&peek));
+		ASSERT_FALSE(stream.Next(&next));
+		ASSERT_EQ(peek, next);
+	}
+
 	TEST_F(TokenizerStreamTest, TestPeekEmpty) {
 		Unicode::UString test;
 		CSS::TokenizerStream stream(test);
@@ -39,6 +73,40 @@ namespace CSS {
 
 		ASSERT_FALSE(stream.Peek(nullptr, std::rand()));
 		ASSERT_FALSE(stream.Peek(&destination, std::rand()));
+	}
+
+	TEST_F(TokenizerStreamTest, TestPeekFirstSemantics) {
+		Unicode::UString test("ABC");
+		CSS::TokenizerStream stream(test);
+
+		char local = 'Q';
+		destination = 'O';
+		ASSERT_TRUE(stream.Peek(&local));
+		ASSERT_TRUE(stream.Peek(&destination, 0));
+		ASSERT_EQ(local, destination);
+	}
+
+	TEST_F(TokenizerStreamTest, TestPeek) {
+		Unicode::UString test("ABC");
+		CSS::TokenizerStream stream(test);
+
+		ASSERT_TRUE(stream.Peek(&destination, 0));
+		ASSERT_EQ(destination, 'A');
+		ASSERT_TRUE(stream.Peek(&destination, 1));
+		ASSERT_EQ(destination, 'B');
+		ASSERT_TRUE(stream.Peek(&destination, 2));
+		ASSERT_EQ(destination, 'C');
+		ASSERT_FALSE(stream.Peek(&destination, 3));
+	}
+
+	TEST_F(TokenizerStreamTest, TestPeekEndSemantics) {
+		Unicode::UString test("G");
+		CSS::TokenizerStream stream(test);
+
+		ASSERT_TRUE(stream.Peek(&destination, 0));
+		ASSERT_EQ(destination, 'G');
+		ASSERT_FALSE(stream.Peek(&destination, 5));
+		ASSERT_EQ(destination, 'G');
 	}
 
 } // namespace CSS
