769a1040fb2a53a3b545034de330bb9b7df15030
Tristan <code@thewoosh.me>
2020-08-14 19:37:09 +0200

    [Test] (CSS::Tokenizer::ConsumeToken) Initial Commit

---
+++ testing/parser/css/tokenizer_consumetoken.cpp
@@ -0,0 +1,18 @@
+/**
+ * Copyright (C) 2020 Tristan. All Rights Reserved.
+ * This file is licensed under the BSD 2-Clause license.
+ * See the COPYING file for licensing information.
+ */
+
+namespace CSS {
+
+	class TokenizerConsumeToken : public ::testing::Test {
+	public:
+		Context context { &ParseErrorTester::ReporterEndpoint };
+	};
+
+	TEST_F(TokenizerConsumeToken, TestEmptyQuotationMarkEnding) {
+	}
+
+} // namespace CSS
+
