ce0278dace95c34fb1e2f338d1c14cd02c3d8938
Tristan <code@thewoosh.me>
2020-08-14 19:42:03 +0200

    [Test] (CSS::Tokenizer::ConsumeToken) Add TestWhitespace

--- testing/parser/css/tokenizer_consumetoken.cpp
+++ testing/parser/css/tokenizer_consumetoken.cpp
@@ -9,10 +9,19 @@ namespace CSS {
 	class TokenizerConsumeToken : public ::testing::Test {
 	public:
 		Context context { &ParseErrorTester::ReporterEndpoint };
-	};
 
-	TEST_F(TokenizerConsumeToken, TestEmptyQuotationMarkEnding) {
-	}
+		void
+		TestWhitespace(const Unicode::UString &string) {
+			Tokenizer tokenizer(context, string);
+			tokenizer.stream.Skip();
+			ASSERT_TRUE(tokenizer.ConsumeToken(string[0]));
+			ASSERT_FALSE(ParseErrorTester::WasParseErrorFired());
+			ASSERT_EQ(tokenizer.tokens.size(), 1);
+			ASSERT_EQ(tokenizer.tokens[0].type, TokenType::WHITESPACE);
+			ASSERT_TRUE(std::holds_alternative<std::nullptr_t>(tokenizer.tokens[0].data));
+			ASSERT_EQ(std::get<std::nullptr_t>(tokenizer.tokens[0].data), nullptr);
+		}
+	};
 
 } // namespace CSS
 
