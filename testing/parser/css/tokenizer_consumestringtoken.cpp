a28bc276a5665113458c566a2f90ace689f26fbd
Tristan <code@thewoosh.me>
2020-08-14 14:18:08 +0200

    [Test] (CSS::Tokenizer::ConsumeStringToken) Add TestCalledByConsumeTokenApostrophe

--- testing/parser/css/tokenizer_consumestringtoken.cpp
+++ testing/parser/css/tokenizer_consumestringtoken.cpp
@@ -139,4 +139,19 @@ namespace CSS {
 		CompareCodePointVectors(data->codePoints, expected);
 	}
 
+	TEST_F(TokenizerConsumeStringToken, TestCalledByConsumeTokenApostrophe) {
+		const Unicode::UString input{ '\'', 'T', 'h', 'i', 's', '\'' };
+		const std::vector<Unicode::CodePoint> expected{ 'T', 'h', 'i', 's' };
+		Unicode::CodePoint character;
+		Tokenizer tokenizer(context, input);
+		ASSERT_TRUE(tokenizer.stream.Next(&character));
+		ASSERT_TRUE(tokenizer.ConsumeToken(character));
+		ASSERT_FALSE(ParseErrorTester::WasParseErrorFired());
+		ASSERT_EQ(tokenizer.tokens.size(), 1);
+		ASSERT_EQ(tokenizer.tokens[0].type, TokenType::STRING);
+		const auto *data = std::get_if<TokenCodePointsData>(&tokenizer.tokens[0].data);
+		ASSERT_NE(data, nullptr);
+		CompareCodePointVectors(data->codePoints, expected);
+	}
+
 } // namespace CSS
