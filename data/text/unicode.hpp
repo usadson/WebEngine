85e54db376b3d95fd55ba123074ffccd4a620517
Tristan <code@thewoosh.me>
2020-08-14 14:52:45 +0200

    [Unicode] Add IsDigit

--- data/text/unicode.hpp
+++ data/text/unicode.hpp
@@ -163,6 +163,11 @@ namespace Unicode {
 		return (character >= 0x41 && character <= 0x5A) || (character >= 0x61 && character <= 0x7A);
 	}
 
+	[[nodiscard]] inline bool
+	IsDigit(CodePoint character) {
+		return character >= Unicode::DIGIT_ZERO && character <= Unicode::DIGIT_NINE;
+	}
+
 	inline bool
 	IsASCIIAlphaNumeric(CodePoint character) {
 		return (character >= 0x30 && character <= 0x39) || (character >= 0x41 && character <= 0x5A)
