f3cf5609a1715c09da02cd9d00fcd0b59b237164
Tristan <code@thewoosh.me>
2020-08-16 17:45:44 +0200

    [Test] (CSS::Tokenizer::ConsumeToken) Use expect instead of assert in some cases of TestHash

--- testing/parser/css/tokenizer_consumetoken.cpp
+++ testing/parser/css/tokenizer_consumetoken.cpp
@@ -50,10 +50,10 @@ namespace CSS {
 	TEST_F(TokenizerConsumeToken, TestHash) {
 		const Unicode::UString string{ ' ' };
 		Tokenizer tokenizer(context, string);
-		ASSERT_TRUE(tokenizer.ConsumeToken(Unicode::NUMBER_SIGN));
-		ASSERT_FALSE(ParseErrorTester::WasParseErrorFired());
+		EXPECT_TRUE(tokenizer.ConsumeToken(Unicode::NUMBER_SIGN));
+		EXPECT_FALSE(ParseErrorTester::WasParseErrorFired());
 		ASSERT_EQ(tokenizer.tokens.size(), 1);
-		ASSERT_EQ(tokenizer.tokens[0].type, TokenType::DELIM);
+		EXPECT_EQ(tokenizer.tokens[0].type, TokenType::DELIM);
 		auto *data = std::get_if<Unicode::CodePoint>(&tokenizer.tokens[0].data);
 		ASSERT_NE(data, nullptr);
 		EXPECT_EQ(*data, Unicode::NUMBER_SIGN);
