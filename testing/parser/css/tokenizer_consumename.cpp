3e4c4afd24407e126c8535e7f6fc16aae0f56ae2
Tristan <code@thewoosh.me>
2020-08-16 21:05:57 +0200

    [Test] (CSS::Tokenizer::ConsumeName) Add TestSimple

--- testing/parser/css/tokenizer_consumename.cpp
+++ testing/parser/css/tokenizer_consumename.cpp
@@ -15,9 +15,22 @@
 
 namespace CSS {
 
-	class TokenizerConsumeStringToken : public ::testing::Test {
+	class TokenizerNameToken : public ::testing::Test {
 	public:
 		Context context { &ParseErrorTester::ReporterEndpoint };
 	};
+
+	TEST_F(TokenizerNameToken, TestSimple) {
+		const Unicode::UString input("test-string ");
+		Unicode::CodePoint codePoint;
+		std::vector<Unicode::CodePoint> output;
+
+		Tokenizer tokenizer(context, input);
+		ASSERT_TRUE(tokenizer.ConsumeName(output));
+		EXPECT_FALSE(ParseErrorTester::WasParseErrorFired());
+		EXPECT_TRUE(tokenizer.tokens.empty());
+		EXPECT_TRUE(tokenizer.stream.Next(&codePoint));
+		EXPECT_EQ(codePoint, Unicode::SPACE);
+	}
 
 } // namespace CSS
