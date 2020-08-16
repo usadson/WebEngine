179207b4315ce6a377eb75851e11bb9b66e2def6
Tristan <code@thewoosh.me>
2020-08-16 21:14:12 +0200

    [Test] (CSS::Tokenizer::ConsumeName) Add TestEscapes test

--- testing/parser/css/tokenizer_consumename.cpp
+++ testing/parser/css/tokenizer_consumename.cpp
@@ -35,4 +35,19 @@ namespace CSS {
 		EXPECT_EQ(output, expected);
 	}
 
+	TEST_F(TokenizerNameToken, TestEscapes) {
+		const Unicode::UString input("\\test ");
+		const std::vector<Unicode::CodePoint> expected { 't', 'e', 's', 't' };
+		Unicode::CodePoint codePoint;
+		std::vector<Unicode::CodePoint> output;
+
+		Tokenizer tokenizer(context, input);
+		ASSERT_TRUE(tokenizer.ConsumeName(output));
+		EXPECT_FALSE(ParseErrorTester::WasParseErrorFired());
+		EXPECT_TRUE(tokenizer.tokens.empty());
+		EXPECT_TRUE(tokenizer.stream.Next(&codePoint));
+		EXPECT_EQ(codePoint, Unicode::SPACE);
+		EXPECT_EQ(output, expected);
+	}
+
 } // namespace CSS
