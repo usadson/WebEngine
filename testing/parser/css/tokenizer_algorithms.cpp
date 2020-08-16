b4aad27f32d6452ae2fa8f79c26196a0c5025b96
Tristan <code@thewoosh.me>
2020-08-16 19:48:50 +0200

    [Test] (CSS::TokenizerAlgorithms) Add IsWhitespace test

--- testing/parser/css/tokenizer_algorithms.cpp
+++ testing/parser/css/tokenizer_algorithms.cpp
@@ -7,7 +7,7 @@
 #include <gtest/gtest.h>
 
 #include "data/text/unicode.hpp"
-#include "parser/css/tokenizer_algoritms.hpp"
+#include "parser/css/tokenizer_algorithms.hpp"
 #include "parser/css/tokenizer_stream.hpp"
 
 namespace CSS {
@@ -16,6 +16,15 @@ namespace CSS {
 	public:
 		Unicode::CodePoint destination = 'A';
 	};
+
+	TEST_F(TokenizerAlgorithmsTest, IsWhitespace) {
+		EXPECT_TRUE(IsWhitespace(Unicode::CHARACTER_TABULATION));
+		EXPECT_TRUE(IsWhitespace(Unicode::LINE_FEED));
+		EXPECT_TRUE(IsWhitespace(Unicode::SPACE));
+		EXPECT_FALSE(IsWhitespace(Unicode::CARRIAGE_RETURN));
+		EXPECT_FALSE(IsWhitespace(Unicode::FORM_FEED));
+		EXPECT_FALSE(IsWhitespace(Unicode::NULL_CHARACTER));
+	}
 
 } // namespace CSS
 
