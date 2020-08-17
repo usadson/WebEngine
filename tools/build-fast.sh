69b96e6ed75eb9b7c293e9173ae97ebfbef0b73a
Tristan <code@thewoosh.me>
2020-08-17 23:47:27 +0200

    [Tooling] (BuildFast) Compile core objects first

--- tools/build-fast.sh
+++ tools/build-fast.sh
@@ -12,9 +12,15 @@ threads=$(grep -c ^processor /proc/cpuinfo | awk '{print $1 * 1.5}')
 # Create the appropriate output folders:
 make bin/test.txt
 
-# Create the full thing AFAP.
+# Create the base first:
+make -j$threads objects
+
+# Create the rest after that.
+# This will ensure all the core components are compiled before executable
+# building is performed (otherwise an executable may be linked before it is
+# built).
 make -j$threads all
 
-# If the previous failed, run the following to ensure everything is compiled
-# correctly:
+# If the previous commands failed, run the following to ensure everything is
+# compiled correctly:
 make
