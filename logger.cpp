727f1c6c9fb5781df4e2b60f6f6165958f3ec583
Tristan <code@thewoosh.me>
2020-08-18 00:10:34 +0200

    [Logger] Make templated

--- logger.cpp
+++ logger.cpp
@@ -5,61 +5,10 @@
  */
 
 #include "logger.hpp"
-
-#include <iostream>
-
-#include <cstdlib>
-
-static bool shouldLog = true;
 static void (*abortFunction)() = &std::terminate;
 
 void
-Logger::Error(const std::string &source, const std::string &message) noexcept {
-	if (shouldLog) {
-		std::cerr << "\033[1;31m[" << source << "] [ERROR] " << message << "\033[1;0m" << std::endl;
-	}
-}
-
-void
-Logger::Debug(const std::string &source, const std::string &message) noexcept {
-	if (shouldLog) {
-		std::cout << "\033[1;35m[" << source << "] [DEBUG] " << message << "\033[1;0m" << std::endl;
-	}
-}
-
-void
-Logger::Info(const std::string &source, const std::string &message) noexcept {
-	if (shouldLog) {
-		std::cout << '[' << source << "] [INFO] " << message << std::endl;
-	}
-}
-
-void
-Logger::Severe(const std::string &source, const std::string &message) noexcept {
-	if (shouldLog) {
-		std::cerr << "\033[1;91m[" << source << "] [SEVERE] " << message << "\033[1;0m" << std::endl;
-	}
-}
-
-void
-Logger::Success(const std::string &source, const std::string &message) noexcept {
-	if (shouldLog) {
-		std::cout << "\033[1;32m[" << source << "] [SUCCESS] " << message << "\033[1;0m" << std::endl;
-	}
-}
-
-void
-Logger::Warning(const std::string &source, const std::string &message) noexcept {
-	if (shouldLog) {
-		std::cout << "\033[1;33m[" << source << "] [WARNING] " << message << "\033[1;0m" << std::endl;
-	}
-}
-
-void
-Logger::Crash(const std::string &source, const std::string &message) {
-	if (shouldLog) {
-		std::cout << "\033[1;33m[" << source << "] [CRASH] " << message << "\033[1;0m" << std::endl;
-	}
+Logger::RunAbort() {
 	abortFunction();
 }
 
--- logger.hpp
+++ logger.hpp
@@ -6,7 +6,9 @@
  * See the COPYING file for licensing information.
  */
 
-#include <string>
+#include <iostream>
+
+#include <cstdlib>
 
 namespace Logger {
 	/**
@@ -17,33 +19,74 @@ namespace Logger {
 	 * Error("NetworkStack::Start", "Couldn't connect to https://example.org");
 	 */
 
-	void
-	Error(const std::string &, const std::string &) noexcept;
+	static bool shouldLog = true;
 
 	void
-	Debug(const std::string &, const std::string &) noexcept;
-
-	void
-	Info(const std::string &, const std::string &) noexcept;
-
-	void
-	Severe(const std::string &, const std::string &) noexcept;
-
-#ifndef Success
-	void
-	Success(const std::string &, const std::string &) noexcept;
-#endif /* Success */
-
-	void
-	Warning(const std::string &, const std::string &) noexcept;
-
-	void
-	Crash(const std::string &, const std::string &);
+	RunAbort();
 
 	void
 	SetOutputState(bool) noexcept;
 
 	void
 	SetAbortFunction(void (*)()) noexcept;
+
+	template<typename SourceType, typename MessageType>
+	void
+	Error(const SourceType &source, const MessageType &message) noexcept {
+		if (shouldLog) {
+			std::cerr << "\033[1;31m[" << source << "] [ERROR] " << message << "\033[1;0m" << std::endl;
+		}
+	}
+
+	template<typename SourceType, typename MessageType>
+	void
+	Debug(const SourceType &source, const MessageType &message) noexcept {
+		if (shouldLog) {
+			std::cout << "\033[1;35m[" << source << "] [DEBUG] " << message << "\033[1;0m" << std::endl;
+		}
+	}
+
+	template<typename SourceType, typename MessageType>
+	void
+	Info(const SourceType &source, const MessageType &message) noexcept {
+		if (shouldLog) {
+			std::cout << '[' << source << "] [INFO] " << message << std::endl;
+		}
+	}
+
+	template<typename SourceType, typename MessageType>
+	void
+	Severe(const SourceType &source, const MessageType &message) noexcept {
+		if (shouldLog) {
+			std::cerr << "\033[1;91m[" << source << "] [SEVERE] " << message << "\033[1;0m" << std::endl;
+		}
+	}
+
+	#ifndef Success
+	template<typename SourceType, typename MessageType>
+	void
+	Success(const SourceType &source, const MessageType &message) noexcept {
+		if (shouldLog) {
+			std::cout << "\033[1;32m[" << source << "] [SUCCESS] " << message << "\033[1;0m" << std::endl;
+		}
+	}
+	#endif /* Success */
+
+	template<typename SourceType, typename MessageType>
+	void
+	Warning(const SourceType &source, const MessageType &message) noexcept {
+		if (shouldLog) {
+			std::cout << "\033[1;33m[" << source << "] [WARNING] " << message << "\033[1;0m" << std::endl;
+		}
+	}
+
+	template<typename SourceType, typename MessageType>
+	void
+	Crash(const SourceType &source, const MessageType &message) noexcept {
+		if (shouldLog) {
+			std::cout << "\033[1;33m[" << source << "] [CRASH] " << message << "\033[1;0m" << std::endl;
+		}
+		RunAbort();
+	}
 
 } // namespace Logger
