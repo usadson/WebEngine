eaf91970ab081390aabcde10a80c9a3002dd5e77
Tristan <code@thewoosh.me>
2020-08-18 16:27:45 +0200

    [Test] (CSS::Tokenizer::ConvertStringToNumber) Add IntTest

--- testing/parser/css/tokenizer_convertstringtonumber.cpp
+++ testing/parser/css/tokenizer_convertstringtonumber.cpp
@@ -9,6 +9,16 @@ namespace CSS {
 	class TokenizerConvertStringToNumber : public ::testing::Test {
 	public:
 		Context context {&ParseErrorTester::ReporterEndpoint};
+		const Unicode::UString simulatedInput;
+		Tokenizer tokenizer{ context, simulatedInput };
 	};
+
+	TEST_F(TokenizerConvertStringToNumber, IntTest) {
+		const std::vector<Unicode::CodePoint> vec{ '1' };
+		const std::variant<std::monostate, std::int64_t, double> result = tokenizer.ConvertStringToNumber(vec);
+		const auto *asDouble = std::get_if<double>(&result);
+		EXPECT_NE(asDouble, nullptr);
+		EXPECT_EQ(*asDouble, 1);
+	}
 
 } // namespace CSS
