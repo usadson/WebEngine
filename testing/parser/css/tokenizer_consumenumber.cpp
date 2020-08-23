14b53eafc768018c2374d603f62a4d67ecc655fc
Tristan <code@thewoosh.me>
2020-08-23 15:42:25 +0200

    [Test] [CSS::Tokenizer::ConsumeNumber) Initial Commit

--- testing/parser/css/tokenizer_consumenumber.cpp
+++ testing/parser/css/tokenizer_consumenumber.cpp
@@ -1,1 +1,20 @@
- 
+/**
+ * Copyright (C) 2020 Tristan. All Rights Reserved.
+ * This file is licensed under the BSD 2-Clause license.
+ * See the COPYING file for licensing information.
+ */
+
+namespace CSS {
+
+	class TokenizerConsumeNumber : public ::testing::Test {
+	public:
+		Context context {&ParseErrorTester::ReporterEndpoint};
+	};
+
+	TEST_F(TokenizerConsumeNumber, TestInt) {
+		const Unicode::UString input{'1'};
+		Tokenizer tokenizer(context, input);
+		static_cast<void>(tokenizer.ConsumeNumber());
+	}
+
+} // namespace CSS
