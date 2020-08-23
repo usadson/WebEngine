535c5cdf174f59e3a9bda24b1abc455582997e18
Tristan <code@thewoosh.me>
2020-08-23 18:09:13 +0200

    [Test] (CSS::Tokenizer::ConsumeNumericToken) Initial Commit

--- testing/parser/css/tokenizer_consumenumerictoken.cpp
+++ testing/parser/css/tokenizer_consumenumerictoken.cpp
@@ -1,1 +1,24 @@
- 
+/**
+ * Copyright (C) 2020 Tristan. All Rights Reserved.
+ * This file is licensed under the BSD 2-Clause license.
+ * See the COPYING file for licensing information.
+ */
+
+namespace CSS {
+
+	class TokenizerConsumeNumericToken : public ::testing::Test {
+	public:
+		Context context {&ParseErrorTester::ReporterEndpoint};
+		Unicode::UString streamContents {};
+		Tokenizer tokenizer {context, streamContents};
+		const Unicode::UString initialString {};
+
+		void
+		Tokenize(Unicode::UString &&string) {
+			streamContents = std::move(string);
+			tokenizer.stream.SetString(&streamContents);
+			ASSERT_TRUE(tokenizer.ConsumeNumericToken());
+		}
+	};
+
+} // namespace CSS
