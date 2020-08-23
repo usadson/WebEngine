1d4ed8305378844210be4ed8bcadf2a95592e245
Tristan <code@thewoosh.me>
2020-08-23 15:14:06 +0200

    [Test] (CSS::Tokenizer) Add ConsumeNumber

--- testing/parser/css/tokenizer.cpp
+++ testing/parser/css/tokenizer.cpp
@@ -54,6 +54,7 @@ namespace ParseErrorTester {
 #include "tokenizer_consumecomments.cpp"
 #include "tokenizer_consumeescapedcodepoint.cpp"
 #include "tokenizer_consumename.cpp"
+#include "tokenizer_consumenumber.cpp"
 #include "tokenizer_consumestringtoken.cpp"
 #include "tokenizer_consumetoken.cpp"
 #include "tokenizer_convertstringtonumber.cpp"
