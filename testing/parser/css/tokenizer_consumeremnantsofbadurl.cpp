46c213b802a9e2dd7468506bde55b69ac808367f
Tristan <code@thewoosh.me>
2020-08-26 14:49:03 +0200

    [Test] (CSS::Tokenizer::ConsumeRemnantsOfBadURL) Add

---
+++ testing/parser/css/tokenizer_consumeremnantsofbadurl.cpp
@@ -0,0 +1,14 @@
+/**
+ * Copyright (C) 2020 Tristan. All Rights Reserved.
+ * This file is licensed under the BSD 2-Clause license.
+ * See the COPYING file for licensing information.
+ */
+
+namespace CSS {
+
+	class TokenizerConsumeRemnantsOfBadURL : public ::testing::Test {
+	public:
+		Context context {&ParseErrorTester::ReporterEndpoint};
+	};
+
+} // namespace CSS
