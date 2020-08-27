ed444fa06cf8f4d849e2ef56ddc699a791541d16
Tristan <code@thewoosh.me>
2020-08-28 00:33:57 +0200

    [Test] (CSS) tokenizer_consumeurltoken: Add SimpleTest

--- testing/parser/css/tokenizer_consumeurltoken.cpp
+++ testing/parser/css/tokenizer_consumeurltoken.cpp
@@ -11,4 +11,17 @@ namespace CSS {
 		Context context {&ParseErrorTester::ReporterEndpoint};
 	};
 
+	TEST_F(TokenizerConsumeURLToken, SimpleTest) {
+		const Unicode::UString string("https://example.com)");
+		Tokenizer tokenizer(context, string);
+		EXPECT_TRUE(tokenizer.ConsumeURLToken());
+		EXPECT_EQ(tokenizer.stream.CodePointsLeft(), 0);
+		ASSERT_EQ(tokenizer.tokens.size(), 1);
+		const auto &token = tokenizer.tokens[0];
+		ASSERT_EQ(token.type, TokenType::URL);
+		const auto &data = std::get<TokenCodePointsData>(token.data);
+		const std::vector<Unicode::CodePoint> expected = {'h', 't', 't', 'p', 's', ':', '/', '/', 'e', 'x', 'a', 'm', 'p', 'l', 'e', '.', 'c', 'o', 'm'};
+		EXPECT_EQ(data.codePoints, expected);
+	}
+
 } // namespace CSS
