d864e207f20c40bed3d2a154d485e1fd1260e66c
Tristan <code@thewoosh.me>
2020-08-28 18:20:14 +0200

    [Test] (CSS) TokenizerFunctions: Add NumberTest

--- testing/parser/css/tokenizer_functions.cpp
+++ testing/parser/css/tokenizer_functions.cpp
@@ -11,4 +11,15 @@ namespace CSS {
 		Context context {&ParseErrorTester::ReporterEndpoint};
 	};
 
+	TEST_F(TokenizerFunctionTests, NumberTest) {
+		const Unicode::UString input("twelve(12);");
+		Tokenizer tokenizer(context, input);
+		EXPECT_TRUE(tokenizer.Run());
+		ASSERT_EQ(tokenizer.tokens.size(), 4);
+		TestFunction(tokenizer.tokens[0], {'t', 'w', 'e', 'l', 'v', 'e'});
+		TestNumber<CSS::IntegerType>(tokenizer.tokens[1], 12);
+		EXPECT_EQ(tokenizer.tokens[2].type, TokenType::PAREN_CLOSE);
+		EXPECT_EQ(tokenizer.tokens[3].type, TokenType::SEMICOLON);
+	}
+
 } // namespace CSS
