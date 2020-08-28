b0237a148a1e5dac1471fb94c5f06fe6c6f9963f
Tristan <code@thewoosh.me>
2020-08-28 17:32:49 +0200

    [Test] (CSS) Tokenizer/ConsumeIdentLikeToken: Add IdentTest

--- testing/parser/css/tokenizer_consumeidentliketoken.cpp
+++ testing/parser/css/tokenizer_consumeidentliketoken.cpp
@@ -11,4 +11,17 @@ namespace CSS {
 		Context context {&ParseErrorTester::ReporterEndpoint};
 	};
 
+	TEST_F(TokenizerConsumeIdentLikeToken, IdentTest) {
+		const Unicode::UString input("string");
+		const std::vector<Unicode::CodePoint> expected {'s', 't', 'r', 'i', 'n', 'g'};
+		Tokenizer tokenizer(context, input);
+		tokenizer.stream.Skip();
+		EXPECT_TRUE(tokenizer.ConsumeToken(input[0]));
+		EXPECT_EQ(tokenizer.tokens.size(), 1);
+		ASSERT_EQ(tokenizer.tokens[0].type, TokenType::IDENT);
+		const auto *data = std::get_if<TokenCodePointsData>(&tokenizer.tokens[0].data);
+		ASSERT_NE(data, nullptr);
+		EXPECT_EQ(data->codePoints, expected);
+	}
+
 } // namespace CSS
