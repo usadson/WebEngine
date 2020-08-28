10655af527c54ed5ec37e0e12c7efd3bb8f372b7
Tristan <code@thewoosh.me>
2020-08-28 18:16:10 +0200

    [Test] (CSS) TokenizerFunctions: Add TestURL+TestFunction

--- testing/parser/css/tokenizer_functions.cpp
+++ testing/parser/css/tokenizer_functions.cpp
@@ -9,6 +9,22 @@ namespace CSS {
 	class TokenizerFunctionTests : public ::testing::Test {
 	public:
 		Context context {&ParseErrorTester::ReporterEndpoint};
+
+		void
+		TestURL(const Token &token, const std::vector<Unicode::CodePoint> &expected) {
+			ASSERT_EQ(token.type, TokenType::URL);
+			const auto *data = std::get_if<TokenCodePointsData>(&token.data);
+			ASSERT_NE(data, nullptr);
+			EXPECT_EQ(data->codePoints, expected);
+		}
+
+		void
+		TestFunction(const Token &token, const std::vector<Unicode::CodePoint> &expected) {
+			ASSERT_EQ(token.type, TokenType::FUNCTION);
+			const auto *data = std::get_if<TokenCodePointsData>(&token.data);
+			ASSERT_NE(data, nullptr);
+			EXPECT_EQ(data->codePoints, expected);
+		}
 	};
 
 } // namespace CSS
