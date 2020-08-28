36821d028cc17347bf61777212461213948a7c6d
Tristan <code@thewoosh.me>
2020-08-28 17:34:10 +0200

    [Test] (CSS) Tokenizer/ConsumeIdentLikeToken: Add FunctionTest

--- testing/parser/css/tokenizer_consumeidentliketoken.cpp
+++ testing/parser/css/tokenizer_consumeidentliketoken.cpp
@@ -24,4 +24,17 @@ namespace CSS {
 		EXPECT_EQ(data->codePoints, expected);
 	}
 
+	TEST_F(TokenizerConsumeIdentLikeToken, FunctionTest) {
+		const Unicode::UString input("rgb(255, 255, 255)");
+		const std::vector<Unicode::CodePoint> expected {'r', 'g', 'b'};
+		Tokenizer tokenizer(context, input);
+		tokenizer.stream.Skip();
+		EXPECT_TRUE(tokenizer.ConsumeToken(input[0]));
+		EXPECT_EQ(tokenizer.tokens.size(), 1);
+		ASSERT_EQ(tokenizer.tokens[0].type, TokenType::FUNCTION);
+		const auto *data = std::get_if<TokenCodePointsData>(&tokenizer.tokens[0].data);
+		ASSERT_NE(data, nullptr);
+		EXPECT_EQ(data->codePoints, expected);
+	}
+
 } // namespace CSS
