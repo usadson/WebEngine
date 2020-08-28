779042123f103d75c33b3e924727cfab2c1718bb
Tristan <code@thewoosh.me>
2020-08-28 17:27:36 +0200

    [Test] Add tokenizer_consumeidentliketoken.cpp

--- testing/parser/css/tokenizer.cpp
+++ testing/parser/css/tokenizer.cpp
@@ -53,6 +53,7 @@ namespace ParseErrorTester {
 // Make sure to also add them to the ingredients in the Makefile!
 #include "tokenizer_consumecomments.cpp"
 #include "tokenizer_consumeescapedcodepoint.cpp"
+#include "tokenizer_consumeidentliketoken.cpp"
 #include "tokenizer_consumename.cpp"
 #include "tokenizer_consumenumber.cpp"
 #include "tokenizer_consumenumerictoken.cpp"
