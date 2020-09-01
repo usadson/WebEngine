9f372ae1b184a1224102d6126f4501076b9fe4e3
Tristan <code@thewoosh.me>
2020-09-01 20:23:15 +0200

    [CSS] (Parser) Implement ConsumeListOfRules partially

--- parser/css/parser.cpp
+++ parser/css/parser.cpp
@@ -9,14 +9,45 @@
 namespace CSS {
 
 	bool
-	Parser::Algorithms::ConsumeListOfRules(const Tokenizer &tokenizer, bool topFlag, std::vector<Rule> &output) noexcept {
+	Parser::Algorithms::ConsumeListOfRules(const Tokenizer &tokenizer, bool topLevelFlag, std::vector<Rule> &output) noexcept {
+		for (auto it = std::begin(tokenizer.tokens); it != std::end(tokenizer.tokens); ) {
+			const auto &token = *it;
+			bool wasItMutatedManually = false;
+			// NOTE that the subroutines called from the following switch
+			// likely mutates the 'it'erator.
+
+			switch (token.type) {
+				case Token::Type::WHITESPACE:
+					break;
+				case Token::Type::CDC:
+				case Token::Type::CDO:
+					if (!topLevelFlag) {
+						// wasItMutatedManually = true;
+						// if (!ConsumeQualifiedRule(it, output)) { ... }
+					}
+					break;
+				case Token::Type::AT_KEYWORD:
+					// wasItMutatedManually = true;
+					// if (!ConsumeAtRule(it, output)) { ... }
+					break;
+				default:
+					// wasItMutatedManually = true;
+					// if (!ConsumeAtRule(it, output)) { ... }
+					break;
+			}
+
+			if (!wasItMutatedManually) {
+				++it;
+			}
+		}
+
 		return true;
 	}
 
 	std::tuple<bool, Stylesheet>
 	Parser::ParseStylesheet(const Tokenizer &tokenizer) noexcept {
 		Stylesheet sheet;
-		return {ConsumeListOfRules(tokenizer, true, sheet.rules), sheet};
+		return {Algorithms::ConsumeListOfRules(tokenizer, true, sheet.rules), sheet};
 	}
 
 } // namespace CSS
