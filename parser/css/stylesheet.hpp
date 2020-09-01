d4f84446242984259c2513f74e21132ca0515de9
Tristan <code@thewoosh.me>
2020-09-01 16:17:54 +0200

    [CSS] (Stylesheet) Implement basic structure

--- parser/css/stylesheet.hpp
+++ parser/css/stylesheet.hpp
@@ -6,21 +6,13 @@
  * See the COPYING file for licensing information.
  */
 
+#include "parser/css/rule.hpp"
 #include "parser/css/tokenizer.hpp"
 
 namespace CSS {
 
-	// [Spec]
-	// Title: CSS Syntax Module Level 3
-	// Section: 5.1.1.
-	// URL: https://www.w3.org/TR/css-syntax-3/#stylesheet-diagram
-	[[nodiscard]] bool
-	ParseStylesheet(const Tokenizer &) noexcept;
 	struct Stylesheet {
-		using Element = std::variant<Token::Type/* qualified-rule, at-rule*/>;
-		// TokenType can be WHITESPACE, CDC and CDO
-		
-		std::vector<Element> elements;
+		std::vector<Rule> rules;
 	};
 
 } // namespace CSS::Parser
