46544c044950754f220338082f0d32d6493fdc05
Tristan <code@thewoosh.me>
2020-09-01 11:00:41 +0200

    [CSS] (Stylesheet) Initial Commit

---
+++ parser/css/stylesheet.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+/**
+ * Copyright (C) 2020 Tristan. All Rights Reserved.
+ * This file is licensed under the BSD 2-Clause license.
+ * See the COPYING file for licensing information.
+ */
+
+#include "parser/css/tokenizer.hpp"
+
+namespace CSS {
+
+	// [Spec]
+	// Title: CSS Syntax Module Level 3
+	// Section: 5.1.1.
+	// URL: https://www.w3.org/TR/css-syntax-3/#stylesheet-diagram
+	[[nodiscard]] bool
+	ParseStylesheet(const Tokenizer &) noexcept;
+	struct Stylesheet {
+		using Element = std::variant<TokenType/* qualified-rule, at-rule*/>;
+		// TokenType can be WHITESPACE, CDC and CDO
+		
+		std::vector<Element> elements;
+	};
+
+} // namespace CSS::Parser
