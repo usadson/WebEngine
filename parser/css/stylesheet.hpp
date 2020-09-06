5400d7a82b47a818d5b634b12ebfa091080b7a85
Tristan <code@thewoosh.me>
2020-09-06 15:05:46 +0200

    [CSS] (Stylesheet) Add subtypes

--- parser/css/stylesheet.hpp
+++ parser/css/stylesheet.hpp
@@ -6,11 +6,30 @@
  * See the COPYING file for licensing information.
  */
 
+#include <memory>
+#include <variant>
 #include <vector>
 
+#include "data/text/ustring.hpp"
 #include "parser/css/rule.hpp"
+#include "parser/css/token.hpp"
 
 namespace CSS {
+
+	struct ComponentValue;
+
+	struct Block {
+		std::vector<ComponentValue> list;
+	};
+
+	struct Function {
+		Unicode::UString name;
+		std::vector<ComponentValue> values;
+	};
+
+	struct ComponentValue {
+		std::variant<Token, Function, Block> value;
+	};
 
 	struct Stylesheet {
 		std::vector<Rule> rules;
