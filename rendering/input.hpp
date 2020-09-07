0a2804dd5c89e2805acf56c7763bad2660a411bb
Tristan <code@thewoosh.me>
2020-09-07 14:43:21 +0200

    [Rendering] (Input) Initial Commit

---
+++ rendering/input.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+/**
+ * Copyright (C) 2020 Tristan. All Rights Reserved.
+ * This file is licensed under the BSD 2-Clause license.
+ * See the COPYING file for licensing information.
+ */
+
+namespace Rendering {
+
+	enum class MouseButton {
+		LEFT, MIDDLE, RIGHT
+	};
+
+} // namespace Rendering
