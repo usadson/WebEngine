0e376736f3567256e0328d46e773c1a48647e485
Tristan <code@thewoosh.me>
2020-09-07 12:32:40 +0200

    [Rendering] Add view.hpp

---
+++ rendering/view.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+/**
+ * Copyright (C) 2020 Tristan. All Rights Reserved.
+ * This file is licensed under the BSD 2-Clause license.
+ * See the COPYING file for licensing information.
+ */
+
+namespace Rendering {
+
+	// The view is the manager of contents in a tab.
+	class View {
+	public:
+	};
+
+} // namespace Rendering
