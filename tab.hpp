0c61f8e7b7504119d0dc4dc0e8716cd6fed5d7e4
Tristan <code@thewoosh.me>
2020-09-07 12:34:10 +0200

    [Tab] Initial Commit

---
+++ tab.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+/**
+ * Copyright (C) 2020 Tristan. All Rights Reserved.
+ * This file is licensed under the BSD 2-Clause license.
+ * See the COPYING file for licensing information.
+ */
+
+#include "rendering/view.hpp"
+
+class Tab {
+public:
+	View view{this};
+};
