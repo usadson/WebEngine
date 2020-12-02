493494c9bf3061e2f29f026096fc3da0751b165e
Tristan <code@thewoosh.me>
2020-12-03 00:35:20 +0100

    Port to CMake

--- parser/html/insertion_mode.hpp
+++ parser/html/insertion_mode.hpp
@@ -7,6 +7,7 @@
  */
 
 #include <map>
+#include <string>
 
 namespace HTML {
 	enum class InsertionModeType {
--- parser/html/state.hpp
+++ parser/html/state.hpp
@@ -7,6 +7,7 @@
  */
 
 #include <map>
+#include <string>
 
 namespace HTML::Tokenizer {
 	enum class ParserState {
